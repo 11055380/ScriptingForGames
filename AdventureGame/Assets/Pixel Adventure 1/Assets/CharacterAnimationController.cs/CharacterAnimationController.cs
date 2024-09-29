14ace86e52ad4335e667cc151e67a1a13603cb89
Gail <11055380@uvu.edu>
2024-09-29 15:53:06 -0600

    Rest of animations on scene, codes copied down

--- AdventureGame/Assets/Pixel Adventure 1/Assets/CharacterAnimationController.cs/CharacterAnimationController.cs
+++ AdventureGame/Assets/Pixel Adventure 1/Assets/CharacterAnimationController.cs/CharacterAnimationController.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterAnimationController : MonoBehaviour
 {
+    
+    private Animator animator;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleAnimations();
+    }
+    
+    private void HandleAnimations()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetTrigger("Run");
+        }
+        else
+        {
+            animator.SetTrigger("Idle");
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("Jump");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("WallJump");
+        }
     }
 }
